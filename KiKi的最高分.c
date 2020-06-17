867d1fcd2ab53e18a0ad73d5a7480ccadb31aafe
DONGJIE-DJ <66857950+DONGJIE-DJ@users.noreply.github.com>
2020-06-17 12:46:07 +0800

    Add files via upload

---
+++ KiKi的最高分.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int c,g,y;
+    scanf("%d %d %d",&c,&g,&y);
+    int max=c>=g?c:g;
+    if(y<=max)
+    {
+
+    }
+    else(max=y);
+    printf("%d",max);
+    return 0;
+}
